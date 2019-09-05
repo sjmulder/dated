c009c7ce0a0be0f427e9a4b065afcb98f3a34f81
Sijmen J. Mulder <ik@sjmulder.nl>
2019-09-05 11:53:48 +0200

    Relicense to 2-clause BSD

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,13 @@
+1.0.2 (2019-09-05)
+------------------
+
+ - Relicensed to 2-clause BSD.
+
 1.0.1 (2018-11-08)
 ------------------
 
  - Fixed default format string.
  - Now pledge()d on OpenBSD.
-
 
 1.0 (2018-11-07)
 ----------------
--- dated.c
+++ dated.c
@@ -1,21 +1,3 @@
-/*
- * dated.c
- * Copyright (c) 2018, Sijmen J. Mulder <ik@sjmulder.nl>
- *
- * dated is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * dated is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with dated. If not, see <https://www.gnu.org/licenses/>.
- */
-
 #include <sys/time.h>
 #include <sys/select.h>
 #include <unistd.h>
